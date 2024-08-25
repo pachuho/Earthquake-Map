db4d03ca60fba07742a207e4b2fda5dcfb371d91
pachuho <juho7410@gmail.com>
2024-08-25 23:16:26 +0900

    Update ButtonToggleGroup to GenericType

--- app/src/main/java/com/pachuho/earthquakemap/ui/screen/map/components/settings/ButtonToggleGroup.kt
+++ app/src/main/java/com/pachuho/earthquakemap/ui/screen/map/components/settings/ButtonToggleGroup.kt
@@ -1,6 +1,7 @@
 package com.pachuho.earthquakemap.ui.screen.map.components.settings
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,19 +10,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import com.naver.maps.map.compose.MapType
 
 @Composable
-fun ButtonToggleGroup(
-    currentMapType: SettingMapType,
-    onClick: (mapType: SettingMapType) -> Unit = {}
+fun <T> ButtonToggleGroup(
+    items: List<T>,
+    currentItem: T,
+    textStyle: TextStyle = MaterialTheme.typography.titleMedium,
+    padding: Int = 10,
+    onClick: (mapType: T) -> Unit = {}
 ) {
-    val items = SettingMapType.getMapType()
     val cornerRadius = 8.dp
 
     Row {
@@ -31,13 +38,14 @@ fun ButtonToggleGroup(
                     0 ->
                         Modifier
                             .offset(0.dp, 0.dp)
-                            .zIndex(if (currentMapType.ordinal == index) 1f else 0f)
+                            .zIndex(if (items.indexOf(currentItem) == index) 1f else 0f)
 
                     else ->
                         Modifier
                             .offset((-1 * index).dp, 0.dp)
-                            .zIndex(if (currentMapType.ordinal == index) 1f else 0f)
+                            .zIndex(if (items.indexOf(currentItem) == index) 1f else 0f)
                 },
+                contentPadding = PaddingValues(padding.dp),
                 onClick = {
                     onClick(item)
                 },
@@ -62,13 +70,13 @@ fun ButtonToggleGroup(
                     )
                 },
                 border = BorderStroke(
-                    1.dp, if (currentMapType.ordinal == index) {
+                    1.dp, if (items.indexOf(currentItem) == index) {
                         MaterialTheme.colorScheme.primary
                     } else {
                         Color.DarkGray.copy(alpha = 0.75f)
                     }
                 ),
-                colors = if (currentMapType.ordinal == index) {
+                colors = if (items.indexOf(currentItem) == index) {
                     // selected colors
                     ButtonDefaults.outlinedButtonColors(
                         containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f),
@@ -83,8 +91,13 @@ fun ButtonToggleGroup(
                 }
             ) {
                 Text(
-                    text = item.value,
-                    color = if (currentMapType.ordinal == index) {
+                    text = when(currentItem) {
+                        is SettingMapType -> (item as SettingMapType).value
+                        is SettingDateType -> (item as SettingDateType).value
+                        else -> item.toString()
+                    },
+                    style = textStyle,
+                    color = if (items.indexOf(currentItem) == index) {
                         Color.White
                     } else {
                         Color.DarkGray.copy(alpha = 0.9f)
@@ -95,36 +108,28 @@ fun ButtonToggleGroup(
     }
 }
 
-enum class SettingMapType(val value: String) {
-    Basic("기본"),
-    Navi("네비게이션"),
-    Hybrid("위성"),
-    Terrain("지형도");
-
-    companion object {
-        fun getMapType(): List<SettingMapType> {
-            return entries.map { it }
-        }
-
-        fun SettingMapType.find(): MapType {
-            return entries
-                .find { this == it }
-                ?.let {
-                    when (it) {
-                        Basic -> MapType.Basic
-                        Navi -> MapType.Navi
-                        Hybrid -> MapType.Hybrid
-                        Terrain -> MapType.Terrain
-                    }
-                } ?: run {
-                MapType.Basic
-            }
-        }
+@Composable
+@Preview(showBackground = true)
+fun DateButtonToggleGroupPreview() {
+    var currentDateType by remember { mutableStateOf(SettingDateType.OneYear) }
+    ButtonToggleGroup(
+        items = SettingDateType.getAll(),
+        currentItem = currentDateType,
+        textStyle = MaterialTheme.typography.labelLarge,
+        padding = 6
+    ) {
+        currentDateType = it
     }
 }
 
 @Composable
 @Preview(showBackground = true)
-fun MaterialButtonToggleGroupPreview() {
-    ButtonToggleGroup(SettingMapType.Basic) {}
+fun MapButtonToggleGroupPreview() {
+    var currentItem by remember { mutableStateOf(SettingMapType.Basic) }
+    ButtonToggleGroup(
+        items = SettingMapType.getAll(),
+        currentItem = currentItem
+    ) {
+        currentItem = it
+    }
 }
