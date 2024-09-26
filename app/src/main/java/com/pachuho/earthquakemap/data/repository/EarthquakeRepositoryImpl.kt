bca22ae2fadaadd7e94c8b27cb21f87447ee0b18
pachuho <juho7410@gmail.com>
2024-09-26 23:38:21 +0900

    Fix: function refactoring, bug fix

--- app/src/main/java/com/pachuho/earthquakemap/data/repository/EarthquakeRepositoryImpl.kt
+++ app/src/main/java/com/pachuho/earthquakemap/data/repository/EarthquakeRepositoryImpl.kt
@@ -9,10 +9,11 @@ import com.pachuho.earthquakemap.data.model.Earthquake
 import com.pachuho.earthquakemap.data.remote.EarthquakeService
 import com.pachuho.earthquakemap.data.utils.NetworkFailureException
 import com.pachuho.earthquakemap.data.utils.isToday
-import com.squareup.moshi.JsonDataException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import timber.log.Timber
-import java.net.SocketTimeoutException
 import java.time.LocalDateTime
 import javax.inject.Inject
 
@@ -24,97 +25,100 @@ class EarthquakeRepositoryImpl @Inject constructor(
 ) : EarthquakeRepository {
 
     override fun getEarthquakes(oauthKey: String) = flow {
-        if (earthquakeDao.getDataCount() > 0) { // 데이터가 있는 경우
-            Timber.i("data is exist")
-            updatePreferencesDataSource.lastUpdateDateTime?.let {
-                if (it.isToday()) { // 오늘 업데이트 된 경우
-                    Timber.i("data is updated today")
-                    emit(earthquakeDao.getEarthquakes())
-                    return@flow
-                } else {
-                    Timber.i("data isn't updated today")
-                    earthquakeDao.getLatestEarthquake()?.let { earthquake ->
-                        // 업데이트 날짜는 다르지만 최신 데이터를 가지고 있는 경우
-                        if (earthquake.EQ_ID == earthquakeService.fetchEarthquakes(oauthKey, 1, 1)
-                                .body()?.tbEqkKenvinfo?.rows?.first()?.EQ_ID
-                        ) {
-                            Timber.i("data is latest")
-                            emit(earthquakeDao.getEarthquakes())
-                            updateCheckDate()
-                            return@flow
-                        }
-                    }
-                }
-            }
+        if (earthquakeDao.getDataCount() <= 0) {
+            emit(emptyList())
+            downloadNewEarthquakes(oauthKey)
+            return@flow
+        }
+        Timber.i("data exists")
+
+        if (isDataUpdatedToday()) {
+            emit(earthquakeDao.getEarthquakes())
+            return@flow
+        }
+
+        val earthquake = earthquakeDao.getLatestEarthquake()
+        if (earthquake != null && isLatestData(oauthKey, earthquake.EQ_ID)) {
+            Timber.i("data is latest")
+            emit(earthquakeDao.getEarthquakes())
+            updateCheckDate()
+            return@flow
         }
 
         emit(emptyList())
+        downloadNewEarthquakes(oauthKey)
+    }
+
+    private fun isDataUpdatedToday(): Boolean {
+        return updatePreferencesDataSource.lastUpdateDateTime?.isToday() == true
+    }
+
+    private suspend fun isLatestData(oauthKey: String, localEqId: String): Boolean {
+        val latestEarthquake = withContext(Dispatchers.IO) {
+            earthquakeService.fetchEarthquakes(oauthKey, 1, 1)
+                .body()?.tbEqkKenvinfo?.rows?.first()
+        }
+        return latestEarthquake?.EQ_ID == localEqId
+    }
+
+    private suspend fun FlowCollector<List<Earthquake>>.downloadNewEarthquakes(oauthKey: String) {
         Timber.i("data download start")
 
-        var isDownloading = true
         var index = 1
         val earthquakes = mutableListOf<Earthquake>()
         val lastDataId = earthquakeDao.getLatestEarthquake()?.EQ_ID
+        var isDownloading = true
 
         while (isDownloading) {
             try {
                 val response = earthquakeService.fetchEarthquakes(oauthKey, index, index + 99)
                 index += 100
 
-                when (response.isSuccessful) {
-                    true -> {
-                        Timber.d("getAllEarthquakes, isSuccessful")
-                        response.body()?.tbEqkKenvinfo?.rows?.let {
-                            Timber.d("add data, isSuccessful")
-                            earthquakes.addAll(it)
+                if (response.isSuccessful) {
+                    response.body()?.tbEqkKenvinfo?.rows?.let {
+                        earthquakes.addAll(it)
 
-                            // 중복 데이터가 있는 경우 다운로드 중지
-                            val ids = it.map { it.EQ_ID }
-                            if(ids.contains(lastDataId)) {
+                        // 중복 데이터가 있는 경우 다운로드 중지
+                        if (it.any { it.EQ_ID == lastDataId }) {
+                            withContext(Dispatchers.IO) {
                                 earthquakeDao.insertEarthquakes(earthquakes)
-                                updateCheckDate()
-                                Timber.i("data download finish")
-                                emit(earthquakes)
                             }
-
-                        } ?: run {
-                            Timber.d("getAllEarthquakes, empty.." + "[${response.code()}] - ${response.raw()}")
-                            throw NetworkFailureException(application.getString(R.string.network_error_description_response_empty))
+                            updateCheckDate()
+                            emit(earthquakes)
+                            isDownloading = false
                         }
-                    }
-
-                    false -> {
-                        Timber.e("response failure" + "[${response.code()}] - ${response.raw()}")
-                        throw NetworkFailureException(application.getString(R.string.network_error_description_resonpse_fail))
-                    }
-                }
-            } catch (e: JsonDataException) {
-                Timber.d("getAllEarthquakes, catch: $e")
-                isDownloading = false
-
-                if (earthquakes.isNotEmpty()) {
-                    earthquakeDao.insertEarthquakes(earthquakes)
-                    updateCheckDate()
-                    Timber.i("data download finish")
-                    emit(earthquakes)
+                    } ?: throw NetworkFailureException(application.getString(R.string.network_error_description_response_empty))
                 } else {
-                    Timber.d("getAllEarthquakes, catch: $e")
-                    throw NetworkErrorException(application.getString(R.string.network_error_description_unknown))
+                    throw NetworkFailureException(application.getString(R.string.network_error_description_resonpse_fail))
                 }
-            } catch (e: SocketTimeoutException) {
-                Timber.d("getAllEarthquakes, catch: $e")
-                throw NetworkErrorException(application.getString(R.string.network_error_description_socket_timeout))
+            } catch (e: Exception) {
+                handleDownloadException(e, earthquakes)
+                isDownloading = false
             }
-            catch (e: Exception) {
-                Timber.d("getAllEarthquakes, catch: $e")
-                throw NetworkErrorException(application.getString(R.string.network_error_description_exception))
+        }
+    }
+
+    private suspend fun FlowCollector<List<Earthquake>>.handleDownloadException(
+        e: Exception,
+        earthquakes: List<Earthquake>
+    ) {
+        Timber.e("Error during download: $e")
+        if (earthquakes.isNotEmpty()) {
+            withContext(Dispatchers.IO) {
+                earthquakeDao.insertEarthquakes(earthquakes)
             }
+            updateCheckDate()
+            emit(earthquakes)
+        } else {
+            throw NetworkErrorException(application.getString(R.string.network_error_description_unknown))
         }
     }
 
     private fun updateCheckDate() {
         LocalDateTime.now().run {
-            updatePreferencesDataSource.lastUpdateDateTime = (year.toString() + monthValue.toString().padStart(2, '0') + dayOfMonth.toString().padStart(2, '0')).toInt()
+            updatePreferencesDataSource.lastUpdateDateTime =
+                (year.toString() + monthValue.toString().padStart(2, '0') + dayOfMonth.toString()
+                    .padStart(2, '0')).toInt()
         }
     }
 }
